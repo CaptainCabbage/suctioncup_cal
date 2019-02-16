1b4d185d29832b9434f4fb62aed27ee20a6b1fb5
xianyi <chengxy0418@gmail.com>
2019-02-16 14:30:11 -0500

    ha

--- scripts/auto_cal.py
+++ scripts/auto_cal.py
@@ -9,12 +9,14 @@ import numpy as np
 import tf.transformations as tr
 import rosbag
 import time
-from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import Int32, String
+from threading import Lock
 
 class Calibration():
 
     def __init__(self):
         rospy.loginfo('Waiting for services...')
+        self.lock = Lock()
         rospy.init_node('suctioncup_cal', anonymous=True, disable_signals=True)
         robot_ns = "/abb120/robot_node"
 
@@ -31,14 +33,7 @@ class Calibration():
         #robot_SetJoints = rospy.ServiceProxy(robot_ns + '/robot_SetJoints', robot_SetJoints)
         rospy.loginfo('All services registered.')
 
-        bagname = 'suctioncup_cal'+ str(int(time.time())) + '.bag'
-        self.bag = rosbag.Bag(bagname, mode='w')
 
-        #TODO: get initial cartesian and wrench(close to zeros, need manually adjust)
-        self.init_pos_data = 0
-        self.init_wrench_data = 0
-        self.bag.write('initial position', pos_data)
-        self.bag.write('initial wrench', wrench_data)
 
         #TODO: set robot speed to be slow
 
@@ -54,25 +49,75 @@ class Calibration():
         #wait for a while
         rospy.sleep(1)
         pos = self.robot_GetCartesian()
-        wrench = 0 #TODO
-        pos_data = [pos.x, pos.y, pos.z, pos.q0, pos.qx, pos.qy, pos.qz]
-        wrench_data = []; #TODO
-        # TODO: get wrench data
+        wrench = 0 #TODO: get wrench data
+        time_now = rospy.Time.now()
 
+        pos_data = to_poseStamped(time_now,'',[pos.x, pos.y, pos.z, pos.q0, pos.qx, pos.qy, pos.qz])
+        wrench_data = to_wrenchStamped(time_now,'',[]); #TODO
 
         # record the cartesian and wrench
         self.bag.write('position', pos_data)
         self.bag.write('wrench', wrench_data)
 
+    def auto_calibration(self):
+        #given some p
+        # go_and_record
+        bagname = 'suctioncup_cal'+ str(int(time.time())) + '.bag'
+        rospy.loginfo("Recording bag with name: {0}".format(bagname))
+        self.bag = rosbag.Bag(bagname, mode='w')
+
+        #TODO: get initial cartesian and wrench(close to zeros, need manually adjust)
+        self.init_pos_data = 0
+        self.init_wrench_data = 0
+        self.bag.write('initial position', pos_data)
+        self.bag.write('initial wrench', wrench_data)
+
+        # compute p
+        # go_and_record
 
     def limit_test(self):
         #TODO:
         # go in six directions to and record the forces
         # get all the feasible forces in the convex hull of sampled forces
+        self.rec = Recorder()
+        self.rec.start()
+        self.rec.stop()
 
-    def auto_calibration(self):
 
+class Recorder():
 
+    def __init__(self):
+        self.lock = Lock()
+        self.is_open = False
+
+    def start(self):
+        name_string = 'suctioncup_limit_'+ str(int(time.time())) + '.bag'
+        rospy.loginfo("Recording bag with name: {0}".format(name_string))
+        with self.lock:
+            self.bag = rosbag.Bag (name_string, mode='w')
+            self.is_open = True
+            # TODO: find the topic names, and data type
+            self.netft_subscriber = rospy.Subscriber('/netft/data',WrenchStamped,  self.force_torque_callback)
+            self.robot_subscriber = rospy.Subscriber('/foxbot/robot_CartesianLog', PoseStamped, self.robot_callback)
+
+    def stop(self):
+        rospy.loginfo("Stopping Recording")
+        with self.lock:
+            self.netft_subscriber.unregister()
+            self.robot_subscriber.unregister()
+            self.bag.close()
+            self.is_open = False
+        rospy.loginfo("Recording stopped.")
+
+    def force_torque_callback(self,data):
+        with self.lock:
+            if self.is_open:
+                self.bag.write('netft', data)
+
+    def robot_callback(self, data):
+        with self.lock:
+            if self.is_open:
+                self.bag.write('position', data)
 
 
 def to_poseStamped(t, frameid, p, q):
