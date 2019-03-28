07758f4a05853e4ea1250718f1d70fad22c49219
Xianyi Cheng <xianyic@andrew.cmu.edu>
2019-03-28 10:38:56 -0400

    ja

--- scripts/auto_cal_rotation_self_supervise.py
+++ scripts/auto_cal_rotation_self_supervise.py
@@ -20,6 +20,9 @@ Z_ref = 421.5
 Z_low = 422
 Z_high = 437
 Z_reset = 445
+delta_z = 1
+delta_angle = 0.5
+ITER = 5
 
 #
 RANDOM_SEED = 0
@@ -52,9 +55,10 @@ class RotationTest():
         self.init_cartesian = p
         self.reset_cartesian = [pos.x, pos.y, Z_reset, pos.q0, pos.qx, pos.qy, pos.qz]
         rospy.loginfo('Initialized.')
+        np.random.seed(RANDOM_SEED)
 
     def test(self):
-        np.random.seed(RANDOM_SEED)
+
         print('Start rotation limit test.')
         iter = int(raw_input('Please enter the number of test: '))
         print('Total test time:'),#,end =" ")
@@ -65,6 +69,11 @@ class RotationTest():
             print('Test'),#,end =" ")
             print(i)
             Z_cur = float(raw_input('Please input your inital z (422 - 437): '))
+            if Z_cur < 422:
+                Z_cur = 422
+            elif Z_cur > 437:
+                Z_cur = 437
+
             p = self.init_cartesian
             p[2] = Z_cur
             #print('Press enter to confirm the position: ',p)
@@ -115,7 +124,80 @@ class RotationTest():
         return
 
     def self_supervise(self):
-        np.random.seed(RANDOM_SEED)
+        angle_all = 2*np.ones([16])
+        angle_all[0:3] = 1
+        angle_all[11:16] = 3
+
+        Z_cur = Z_low
+        i = 0
+        count = 0
+        seqid = 0
+
+
+        while Z_cur < Z_high:
+            print('Current z:'),
+            print(Z_cur)
+            p = self.init_cartesian
+            p[2] = Z_cur
+            self.go(p)
+            self.record(seqid)
+            seqid = seqid + 1
+
+            angle_max = angle_all[i]
+            angle = delta_angle
+
+            while angle < angle_max + 0.1:
+                print('Rotation angle: '),
+                print(angle_i)
+                dt_random = np.sin(angle_i*np.pi/180)*np.random.random(ITER)
+                dz_random = -np.sin(angle_i*np.pi/180)*np.random.random(ITER)
+                for j in range(ITER):
+                    dt = dt_random[j]
+                    dz = dz_random[j]
+                    dr = 2*(np.random.rand(1,2)-0.5)
+                    dr = np.append(dr/np.linalg.norm(dr),0)
+                    print('Randomly sampled rotational axis:'),#, end =" ")
+                    print(dr)
+                    print('Tangential translation: '),#,end =" ")
+                    print(dt)
+                    dp = np.append(dt*np.array([dr[1],-dr[0]]),dz)
+                    dR = tr.rotation_matrix(angle*np.pi/180, dr)
+                    dp_compensate = np.array([0,0,-GRIPPER_LENGTH])-np.dot(dR[0:3,0:3],np.array([0,0,-GRIPPER_LENGTH]))
+                    R = np.dot(R0,dR)
+                    quat_i = tr.quaternion_from_matrix(R)
+                    pos_i = dp + dp_compensate + p[0:3]
+                    p_new = np.append(pos_i,quat_i)
+                    print("Check the robot position, press Enter to confirm: ")
+                    raw_input(p_new)
+                    wrench_0 = self.cur_wrench
+                    self.go(p_new)
+                    raw_input('Press Enter to go back: ')
+                    self.go(p)
+
+                    if cmp_netftdata(wrench_0, self.cur_wrench):
+                        print('Good test, record data now.')
+                        self.go(p_new)
+                        self.record(seqid)
+                        self.go(p)
+                        seqid = seqid + 1
+                        count = count + 1
+                    else:
+                        print('Bad test, reset vacuum gripper.')
+                        self.go(self.reset_cartesian)
+                        self.go(p)
+                angle = angle + delta_angle
+
+            Z_cur = Z_cur + delta_z
+            i = i+1
+
+        self.bag.close()
+        print('Test finished, stop recording.')
+        print('Total valid test: '),
+        print(count)
+        self.netft_subscriber.unregister()
+        self.go(self.init_cartesian)
+        return
+
 
     def go(self, p):
         print('Go to:'),#, end =" ")
@@ -123,7 +205,7 @@ class RotationTest():
         self.robot_SetCartesian(*p)
         rospy.sleep(5)
 
-    def record(self, seqid):
+    def record(self,seqid):
         pos = self.robot_GetCartesian()
         time_now = rospy.Time.now()
         pos_data = to_poseStamped(time_now,seqid,[pos.x, pos.y, pos.z], [pos.q0, pos.qx, pos.qy, pos.qz])
@@ -169,7 +251,21 @@ def to_wrenchStamped(t, seqid, f, tau):
 
     return wrenchs
 
+def cmp_netftdata(w0, w1):
+    thr = np.array([0.2,0.2,0.2,0.2,0.2,0.2])
+
+    d0 = np.array([w0.wrench.force.x,w0.wrench.force.y,w0.wrench.force.z,w0.wrench.torque.x,w0.wrench.torque.y,w0.wrench.torque.z])
+    d1 = np.array([w1.wrench.force.x,w1.wrench.force.y,w1.wrench.force.z,w1.wrench.torque.x,w1.wrench.torque.y,w1.wrench.torque.z])
+    d = np.abs(d0 - d1)
+    r = d > thr
+
+    if np.sum(r) > 0:
+        return False
+    else:
+        return True
+
+
 if __name__ == '__main__':
     rt = RotationTest()
     rospy.sleep(1)
-    rt.test()
+    rt.self_supervise()
