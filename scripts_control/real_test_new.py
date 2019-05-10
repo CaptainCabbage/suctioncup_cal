32c0405ea28b84a51107ecd14ba5f3b7c596136b
Xianyi Cheng <chengxy0418@gmail.com>
2019-05-09 23:01:21 -0400

    s

--- scripts_control/real_test_new.py
+++ scripts_control/real_test_new.py
@@ -12,41 +12,47 @@ robot = abbRobot()
 robot.initialize()
 robot.SetSpeed(5,3)
 p0 = np.zeros(7)
-p0[0:3] = task_model.ref_act_traj[0,0:3]
+p0[0:3] = task_model.actual_start[0:3]
 p0[2] = 520
 p0[3:] = [0,1,0,0]
-print('go to: '),
-print(p0)
-raw_input()
+#print('go to: '),
+#print(p0)
+#raw_input()
 
-robot.go(p0)
+#robot.go(p0)
 #raw_input('Please turn on vacuum')
 
 print('go to: '),
 print(task_model.actual_start)
 raw_input()
 robot.go(task_model.actual_start)
-rospy.sleep(5)
+#rospy.sleep(5)
 i=0
 while i < task_model.total_timestep-1:
     print('timestep:',i,'angle', i*30/45)
     task_model.current_timestep = i
 
-    robot_cartesian = robot.get_current_cartesian()
+    robot_cartesian = np.array(robot.get_current_cartesian())
     cur_wrench = task_model.wrench_compensation(robot.current_ft(), robot_cartesian[3:])
     print('cur wrench',cur_wrench)
 
     task_model.state_estimation(cur_wrench, robot_cartesian)
+
     dut = task_model.position_optimal_control(cur_wrench, task_model.ref_obj_vel_traj[i]).reshape(-1)
+
     dx_robot_body = dut[6:12]
+    print(dut)
     dx_robot_spatial = adjointTrans(quat2rotm(robot_cartesian[3:]),robot_cartesian[0:3]).dot(dx_robot_body)
-    robot_ut = np.concatenate(robot_cartesian[0:3] + dx_robot_spatial[0:3], quat_mul(exp2quat(dx_robot_spatial[3:]), robot_cartesian[3:]))
+    robot_ut = np.zeros(7)
+    robot_ut[0:3] = robot_cartesian[0:3] + dx_robot_spatial[0:3]
+    robot_ut[3:] = quat_mul(exp2quat(dx_robot_spatial[3:]), robot_cartesian[3:])
 
     actual_ut = task_model.robot2actual(robot_ut)
 
     print('control input: ', actual_ut,'press enter to confirm, input 0 to reject')
     var = raw_input()
     if var == '0':
+        i+=1
         continue
     robot.go(actual_ut)
     rospy.sleep(2)
